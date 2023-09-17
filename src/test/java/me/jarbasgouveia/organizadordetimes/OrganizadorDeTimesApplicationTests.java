dcd3337d1becf86ceeddbc24b5704e6dacab972b
jjgouveia <gouvik.dev@gmail.com>
2023-09-17 02:42:05 -0300

    Initial commit

---
+++ src/test/java/me/jarbasgouveia/organizadordetimes/OrganizadorDeTimesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jarbasgouveia.organizadordetimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizadorDeTimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
